d619ffd06d69bde0a397c8c5d1c7caf5646f9c34
Claudia Fox <claudiafox95@icloud.com>
2018-11-21 16:49:11 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,19 @@
-
+class CashRegister
+  
+  attr_accessor :discount, :total, :name, :price, :quantity
+  
+  def initialize(staff_discount = 0)
+      @total = 0
+      @discount = staff_discount
+  end
+  
+  def total
+    @total
+  end
+  
+  def add_item(name, price, quantity = 1)
+    
+    
+  end
+    
+end
