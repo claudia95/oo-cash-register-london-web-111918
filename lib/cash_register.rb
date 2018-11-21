faf4fd69fd16928206d028c6f991d1ea47c5ac5c
Claudia Fox <claudiafox95@icloud.com>
2018-11-21 17:04:48 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,7 +18,7 @@ class CashRegister
     
   def apply_discount(total)
     if discount
-      @total = total - total * discount / 100.00
+      @total = total - total * discount.to_f / 100.00
     end
     "After the discount, the total comes to $#{total}."
   end
